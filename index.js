9dd1ebf38e6915aa7f830c1212f804da796ce40b
Thomas Rains <thomasrains01@gmail.com>
2024-10-07 16:45:51 -0700

    submits Phase 1 functions lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(streetValue){
+    return Math.abs(42 - streetValue);
+}
+
+function distanceFromHqInFeet(streetValue) {
+    distanceFromHqInBlocks(streetValue);
+        return 264 * (Math.abs(42 - streetValue));
+  }
+
+function distanceTravelledInFeet(start, destination){
+    return 264 * (Math.abs(destination - start));
+}
+
+function calculatesFarePrice(start, destination) {
+    let distanceInFeet = distanceTravelledInFeet(start, destination);
+    if (distanceInFeet > 2500){
+        return "cannot travel that far"
+    }
+    else if (distanceInFeet >= 2000){
+        return 25
+    }
+    else if (distanceInFeet > 400){
+        return .02 * (distanceInFeet - 400)
+    }
+    else {
+        return 0
+    }
+}
